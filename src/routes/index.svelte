a896b3e448c44cc2288759fb1b041603c1e9e9da
Zach Neibaur <zneibaur@protonmail.com>
2022-07-31 22:46:06 -0600

    Preparing some other TypeScript additions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
   import AddPlayerDialog from '../components/AddPlayerDialog.svelte';
   import { onMount } from 'svelte';
+  // import DeleteDialog from 'src/components/DeleteDialog.svelte';
   let isInactiveOpen: boolean = true;
   let isActiveOpen: boolean = true;
   let maxActivePlayers: number = 7;
   let playTimeLimit: string = '05:00';
 
+  let deleteDialog: HTMLDialogElement;
   let addPlayerDialog: HTMLDialogElement;
   onMount(() => {
+    deleteDialog = document.getElementById('deleteDialog') as HTMLDialogElement;
     addPlayerDialog = document.getElementById('addPlayerDialog') as HTMLDialogElement;
   })
 
   const showModal = () => {
     addPlayerDialog.showModal();
   }
+
+  const deletePerson = () => {
+    deleteDialog.close();
+  }
 </script>
 
 <main>
   <p>Main Sub Watcher Area</p>
+  <!-- <DeleteDialog personToDelete="Zach" deletePerson={deletePerson} /> -->
   <AddPlayerDialog />
   <button class="add" on:click={showModal} >
     Add Player
