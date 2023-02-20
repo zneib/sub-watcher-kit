7d3b4e809021328beb95c278ab6ee6cb0960e3ec
Zach Neibaur <zneibaur@protonmail.com>
2023-02-20 14:37:35 +0000

    One more try

--- src/components/AddPlayerDialog.svelte
+++ src/components/AddPlayerDialog.svelte
@@ -45,7 +45,7 @@
       </div>
       <div class="button-wrapper">
         <button type="submit">Add</button>
-        <button value="cancel" on:click|stopPropagation={closeDialog}>Close</button>
+        <button value="cancel" on:click|stopPropagation|preventDefault={closeDialog}>Close</button>
       </div>
     </form>
   </div>
