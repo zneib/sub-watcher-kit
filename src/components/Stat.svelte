dbd0b8e0dafb70f955af7bffca21b285f19180e4
Zach Neibaur <zneibaur@protonmail.com>
2023-02-27 21:46:15 -0700

    Preparing the styles for the Stat component

--- src/components/Stat.svelte
+++ src/components/Stat.svelte
@@ -15,13 +15,44 @@
   <p class="total">{total}</p>
   <p>{text.toUpperCase()}</p>
   <button class="minusSign" on:click={handleMinus}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <svg class="minusSign" xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" />
-    </svg>
+    </svg>  
   </button>  
   <button class="plusSign" on:click={handlePlus}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <svg class="plusSign" xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
-    </svg>
+    </svg> 
   </button>
 </div>
+
+<style>
+  div.track {
+    display: inline-block;
+    place-items: center;
+    font-size: 9px;
+    margin: 8px 20px 0 20px;
+    text-align: center;
+    position: relative;
+  }
+  div.track > p {
+    margin: 0;
+  }
+  div.track > p.total {
+    font-size: 18px;
+  }
+  svg.minusSign {
+    position: absolute;
+    top: -20px;
+    bottom: 0;
+    left: -20px;
+    margin: auto 0;
+  }
+  svg.plusSign {
+    position: absolute;
+    top: -20px;
+    bottom: 0;
+    right: -20px;
+    margin: auto 0;
+  }
+</style>
