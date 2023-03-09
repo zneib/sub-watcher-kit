404fdb13ac8ec8e66ed7ac2679c4b44a5fcb5e66
Zach Neibaur <zneibaur@protonmail.com>
2023-03-08 17:10:16 -0700

    Preparing stats and games pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,22 +41,36 @@
     </svg>
     <p>LOAD TEAM</p>
   </div> -->
-  <div class="auth-area">
-    {#if $page.data.session}
-      {#if $page.data.session.user?.image}
-        <div style="display: flex">
-          <img class="avatar" src={$page.data.session.user.image} alt="User Avatar">
-          <span class="signedInText">
-            <small>Signed in as</small><br />
-            <strong>{$page.data.session.user?.name ?? "User"}</strong>
-          </span>
-        </div>
+  <div>
+    <div class="headerItem">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+      </svg>
+      <p>STATS</p>      
+    </div>
+    <div class="headerItem">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+      </svg>
+      <p>GAMES</p>    
+    </div>
+    <div class="auth-area">
+      {#if $page.data.session}
+        {#if $page.data.session.user?.image}
+          <div style="display: flex">
+            <img class="avatar" src={$page.data.session.user.image} alt="User Avatar">
+            <span class="signedInText">
+              <small>Signed in as</small><br />
+              <strong>{$page.data.session.user?.name ?? "User"}</strong>
+            </span>
+          </div>
+        {/if}
+          <button on:click={() => signOut()} class="sign-out-btn">Sign out</button>
+        {:else}
+          <span class="notSignedInText">You are not signed in</span>
+          <button class="notSignedInBtn" on:click={() => signIn("github")}>Sign In with GitHub</button>
       {/if}
-        <button on:click={() => signOut()} class="sign-out-btn">Sign out</button>
-      {:else}
-        <span class="notSignedInText">You are not signed in</span>
-        <button class="notSignedInBtn" on:click={() => signIn("github")}>Sign In with GitHub</button>
-    {/if}
+    </div>
   </div>
 </header>
 <hr>
@@ -133,6 +147,22 @@
 
   .notSignedInBtn {
     margin-top: 15px;
+  }
+
+  .headerItem {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    margin-right: 50px;
+    text-align: center;
+  }
+  .headerItem > svg {
+    margin-bottom: 5px;
+  }
+  .headerItem > p {
+    margin: 0;
   }
 
   button.saveGame {
