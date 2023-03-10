64c01509d4d52274d2419dbc93af8c79ecf0248a
Zach Neibaur <zneibaur@protonmail.com>
2023-03-10 15:38:35 -0700

    Removing slow data load and adding active class to current links

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
 	import type { GameType } from '../../global-types';
-  import type { PageData } from './$types';
 
   let games: GameType[] = [];
-  export let data: PageData;
-  console.log(data.games.data)
+  async function getGames() {
+    const res = await fetch(`https://zneib-sub-watcher-api.deno.dev/api/games`)
+    const data = await res.json();
+    games = data;
+    console.log(games);
+  }
+
+  onMount(() => {
+    getGames();
+  })
 </script>
 
 <h1>Games Page</h1>
-<ul>
+<p>Team stuff goes here</p>
+<!-- <ul>
   {#each data?.games?.data as game}
     <li>{game.data.name}</li>
   {/each}
-</ul>
+</ul> -->
