dc32500d23dbfa70b484067832c3387bc11e067d
Zach Neibaur <zneibaur@protonmail.com>
2023-10-27 00:06:16 -0600

    Accessibility fixes

--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -9,15 +9,17 @@
   $: isActivated = player.id === selectedPlayer.id;
 </script>
 
-<div class="name" class:isActivated on:click={() => updateActivePlayer(player)} on:keyup={() => console.log('Player')}>
+<button class="name" class:isActivated on:click={() => updateActivePlayer(player)}>
+  {#if !isActivated}
+    <span class="index">{player.playerNumber}</span>
+  {:else}
+  <button class="bench" on:click={() => removeActivePlayer(player)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>    
+  </button>
+  {/if}
   <div class="nameAndNumber">
-    {#if !isActivated}
-      <span class="index">{player.playerNumber}</span>
-    {:else}
-      <svg xmlns="http://www.w3.org/2000/svg" class="bench" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" on:click={removeActivePlayer(player)} on:keyup={removeActivePlayer(player)}>
-        <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>    
-    {/if}
     <span class="name">{player.playerName}</span>
   </div>
   <div class="stats">
@@ -26,23 +28,25 @@
     <Stat player={player} isActivated={isActivated} text="fouls" total={player.fouls ?? 0}/>
     <Stat player={player} isActivated={isActivated} text="blocks"/>
   </div>
-</div>
+</button>
 
 <style>
   div {
     height: 45px;
   }
-  div.name {
+  button.name {
     display: flex;
     justify-content: space-between;
     align-items: center;
     background: #fff;
+    border: none;
     border-radius: 5px;
     box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.2);
     margin: 10px 0px 10px 30px;
     padding: 5px 10px;
     transition: box-shadow ease-in .2s;
     position: relative;
+    width: 95%;
   }
   div.nameAndNumber {
     display: flex;
@@ -54,14 +58,14 @@
     display: flex;
     justify-content: space-evenly;
   }
-  div.name:hover {
+  button.name:hover {
     box-shadow: 0px 2px 6px 2px rgba(0,0,0,0.2);
     cursor: pointer;
   }
-  div.isActivated {
+  button.isActivated {
     border: 2px solid #ccc;
   }
-  div.isActivated:hover {
+  button.isActivated:hover {
     box-shadow: none;
   }
   span.index {
@@ -80,23 +84,26 @@
     left: -35px;
     margin: auto;
   }
-  svg.bench {
+  button.bench {
     position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
     border-radius: 5px;
-    padding: 5px 10px;
+    /* padding: 5px 10px; */
     top: 0;
     bottom: 0;
     left: -40px;
-    margin: auto;
+    /* margin: auto; */
+    background: transparent;
+    color: #fff;
+    border: none;
   }
 
   @media (prefers-color-scheme: dark) {
-    div.name {
+    button.name {
       color: var(--grey-nine);
       background-color: var(--grey-four);
     }
